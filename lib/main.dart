3ac1d5a15d5710edae1f393627bab189ac6c4aa9
AlGoldyn <al_goldyn@wp.pl>
2025-01-25 20:26:37 +0100

    add_firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:provider/provider.dart';
 import './providers/spell_provider.dart';
 import './providers/spell_slot_provider.dart'; 
 import './screens/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MyApp());
 }
 
